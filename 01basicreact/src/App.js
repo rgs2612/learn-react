3e4ae8e635c35db5d0476c3f739a0fa3c8b996ce
Gowri Shankar <gowrishankar@Gowris-MacBook-Air.local>
2025-03-02 18:14:14 +0530

    basic changes

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,9 @@
 
-
 function App() {
   return (
-    <h1>Classic React App</h1>
+    <>
+      <h1>Classic React App</h1>
+    </>
   );
 }
 
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -9,6 +9,6 @@ const Element = React.createElement(
 
 createRoot(document.getElementById('root')).render(
   <>
-    <Element />
+    {Element}
   </>
 )
