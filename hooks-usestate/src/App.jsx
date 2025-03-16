7253a2dbd33ed6bf869227acc438d003c38728f6
Gowri Shankar <gowrishankar@Gowris-MacBook-Air.local>
2025-03-16 13:40:07 +0530

    simple counter program using useState

---
+++ hooks-usestate/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+
+export default function Counter(){
+  const [count, setCount] = useState(0)
+
+  return (
+     <div className='tutorial'>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+     </div>
+  )
+}
